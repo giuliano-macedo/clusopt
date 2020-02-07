39a235e7fd3bccb6578472fa212d89ce435f1e52
Giuliano Oliveira <giuliano.llpinokio@gmail.com>
2020-02-07 16:42:06 -0400

    leaking

--- src/core/streamkm/test.py
+++ src/core/streamkm/test.py
@@ -1,8 +1,8 @@
 from streamkm import Streamkm
 import numpy as np
 np.random.seed(1)
-model=Streamkm(length=10000,coresetsize=2)
+batch=np.random.rand(10000,2)
 while True:
-	batch=np.random.rand(10000,2)
+	model=Streamkm(length=10000,coresetsize=100)
 	model.batch_online_cluster(batch)
-	# del model
+	del model
