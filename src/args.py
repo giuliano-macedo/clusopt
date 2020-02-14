e1e68863228a85e92f5fc82c97a3629e38615979
Giuliano Oliveira <giuliano.llpinokio@gmail.com>
2020-02-14 08:45:50 -0400

    better argparse, bugfix

--- src/args.py
+++ src/args.py
@@ -1,27 +1,13 @@
-from argparse import ArgumentTypeError,ArgumentParser
+import argparse
 import logging
 from math import ceil,sqrt
 from utils import count_flines
 
-def parse_alg(s):
-	ans={
-		"minibatch":		"minibatch",
-		"minibatchkmeans":	"minibatch",
-		"clustream":		"clustream",
-		"streamkm":			"streamkm",
-		"streamkm++":		"streamkm",
-		"streamkmeans":		"streamkm",
-		"streamkmeans++":	"streamkm"
-	}.get(s.lower(),None)
-	if not ans:
-		raise ArgumentTypeError("invalid algorithm")
-	return ans
 def parse_args():
-	parser=ArgumentParser()
-	parser.add_argument(
-		"algorithm",
-		help="aglortihm to use, availabe are (minibatch,clustream,streamkm)",
-		type=parse_alg
+	parser=argparse.ArgumentParser()
+	subparsers=parser.add_subparsers(
+		title="algorithm",
+		help="which core algorithm will be used"
 	)
 	parser.add_argument(
 		"input",
@@ -47,55 +33,78 @@ def parse_args():
 		default=None,
 		help="lower threshold for kappa set generation, default is sqrt(ceil(batch_size|m|coresetsize))"
 	)
+	parser.add_argument(
+		"-r",
+		"--remote-nodes",
+		type=str,
+		default="./remote_nodes.txt",
+		help="list of remote notes ip txt file path"
+	)
 	#minibatch
 	#------------------------------------------------------------------------------------
-	parser.add_argument(
+	minibatch=subparsers.add_parser(
+		"minibatch",
+		help="MiniBatchKmeans online clustering algorithm"
+	)
+	minibatch.set_defaults(algorithm="minibatch")
+	minibatch.add_argument(
 		"-b",
 		"--batch-size",
 		type=int,
-		help="(minibatch) size of chunks to send to slaves (default 2000)",
+		help="size of chunks to send to slaves (default 2000)",
 		default=2000
 	)
 	#clustream
 	#------------------------------------------------------------------------------------
-	parser.add_argument(
-		"-h",
+	clustream=subparsers.add_parser(
+		"clustream",
+		help="CluStream online clustering algorithm"
+	)
+	clustream.set_defaults(algorithm="clustream")
+	clustream.add_argument(
+		"-H",
 		"--window-range",
 		type=int,
-		help="(clustream) Range of the window (default 100)",
+		help="Range of the window (default 100)",
 		default=100
 	)
-	parser.add_argument(
+	clustream.add_argument(
 		"-m",
 		"--microclusters",
 		type=int,
 		default=1000,
-		help="(clustream) Maximum number of micro clusters to use (default 1000)"
+		help="Maximum number of micro clusters to use (default 1000)"
 	)
-	parser.add_argument(
+	clustream.add_argument(
 		"-t",
 		"--kernel-radius",
 		type=int,
 		default=2,
-		help="(clustream) Multiplier for the kernel radius (default 2)"
+		help="Multiplier for the kernel radius (default 2)"
 	)
 	#streamkm
 	#------------------------------------------------------------------------------------
-	parser.add_argument(
+	streamkm=subparsers.add_parser(
+		"streamkm",
+		help="StreamKm++ online clustering algorithm"
+	)
+	streamkm.set_defaults(algorithm="streamkm")
+	streamkm.add_argument(
 		"-c",
 		"--coreset-size",
 		type=int,
-		help="(streamkm) Number of coresets to use (default 1000)",
+		help="Number of coresets to use (default 1000)",
 		default=1000
 	)
-	parser.add_argument(
+	streamkm.add_argument(
 		"-L",
 		"--length",
 		type=int,
-		help="(streamkm) Number of datapoints to process (default:length of the dataset)",
+		help="Number of datapoints to process (default:length of the dataset)",
 		default=None
 	)
-	args=parser.get_args()
+	args=parser.parse_args()
+	print(args)
 	if args.verbose:
 		logging.basicConfig(format='[%(levelname)s]%(message)s',level=logging.DEBUG)
 	if args.algorithm=="minibatch" and args.lower_threshold==None:
@@ -104,7 +113,7 @@ def parse_args():
 		args.lower_threshold=int(ceil(sqrt(args.microclusters)))
 	elif args.algorithm=="streamkm":
 		if args.lower_threshold==None:
-			args.lower_threshold=int(ceil(sqrt(args.microkernels)))
+			args.lower_threshold=int(ceil(sqrt(args.coreset_size)))
 		if args.length==None:
 			args.length=count_flines(args.input)
 	return args
