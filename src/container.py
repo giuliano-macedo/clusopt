eb63768199a24e46c4c02415cf18ac349e9fe524
Giuliano Oliveira <giuliano.llpinokio@gmail.com>
2020-02-18 10:06:54 -0400

    updated remote node

--- src/container.py
+++ src/container.py
@@ -5,17 +5,21 @@ container.py
 The container node, uses the same slave algorithm, but it creates an auxiliar server first, when the first
 socket connects, it assumes it have a master node executing, so it connects itself as a slave.
 """
-from slave import Slave
+from slave import main
+import logging
 from network import ServerSocket
-import json
+from argparse import ArgumentParser
 
 if __name__=="__main__":
-	with open("config.json") as f:
-		config=json.load(f)
-	print("auxiliar server started",flush=True)
+
+	parser=ArgumentParser()
+	parser.add_argument('-v','--verbose', action='store_true',help="enbale verbose")
+	args=parser.parse_args()
+	if args.verbose:
+		logging.basicConfig(format='[%(levelname)s]%(message)s',level=logging.DEBUG)
+
+	print("auxiliar server started")
 	server=ServerSocket(3523)
 	master=server.accept()
-	print(f"connected to {master.ip}",flush=True)
-	import logging
-	logging.basicConfig(format='[%(levelname)s]%(message)s',level=logging.DEBUG)
-	Slave(**config).run(master)
+	print(f"connected to {master.ip}")
+	main(master)
