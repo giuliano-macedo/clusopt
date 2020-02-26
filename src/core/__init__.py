7af0f793c122a469675dc7d9eb93156b8ef512cd
Giuliano Oliveira <giuliano.llpinokio@gmail.com>
2020-02-26 18:28:06 -0400

    Cache not needed anymore

--- src/core/__init__.py
+++ src/core/__init__.py
@@ -6,6 +6,5 @@ algorithms core components
 
 from namedlist import namedlist
 BucketEntry=namedlist("BucketEntry",["sil","k","counter","msock","timer"])
-from .Cache import Cache
 from .Clusterer import Clusterer
 from .Bucket import Bucket
