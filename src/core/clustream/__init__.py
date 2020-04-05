1db4ffbe2ba5d74851be96a707bc22e3c13158ff
Giuliano Oliveira <giuliano.llpinokio@gmail.com>
2020-04-05 12:03:06 -0400

    clustream init_offline

--- src/core/clustream/__init__.py
+++ src/core/clustream/__init__.py
@@ -1,7 +1,7 @@
 import os
-import sys
+from sklearn.cluster import KMeans
 try:
-	from .clustream import CluStream
+	from .clustream import CluStream as CluStream_
 except ImportError:
 	backup=os.getcwd()
 	print("compiling CluStream")
@@ -9,7 +9,25 @@ except ImportError:
 	os.system("make")
 	os.chdir("..")
 	try:
-		from .clustream import CluStream
+		from .clustream import CluStream as CluStream_
 	except ImportError:
 		raise ImportError("Something went wrong in the compilation")
 	os.chdir(backup)
+
+class CluStream(CluStream_):
+	def init_offline(self,init_points,seed=0,n_init=1):
+		"""
+		initialize microclusters using kmeans++
+
+		Args:
+			init_points (ndarray): points to initialize
+			seed (int): 
+			n_init (init): number of kmeans runs
+		"""
+		cluster_centers=KMeans(
+			n_clusters=self.m,
+			init="k-means++",
+			random_state=seed,
+			n_init=n_init
+		).fit(init_points).cluster_centers_
+		self.init_kernels_offline(cluster_centers,init_points)
