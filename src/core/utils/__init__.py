5e7c372d220e8a41f62bce82aa12ba8b020caced
Giuliano Oliveira <giuliano.llpinokio@gmail.com>
2020-07-26 11:16:43 -0400

    typo

--- src/core/utils/__init__.py
+++ src/core/utils/__init__.py
@@ -1,4 +1,4 @@
 import cppimport
 cppimport.set_quiet(False)
 Silhouette = cppimport.imp("core.utils.silhouette").Silhouette
-DistanceTable = cppimport.imp("core.utils.DistanceTable").DistanceTable
+DistanceTable = cppimport.imp("core.utils.dist_table").DistanceTable
