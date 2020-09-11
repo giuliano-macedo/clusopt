76415ad7593fb7d21ba5cb596e2f021c8bf43447
Giuliano Oliveira <giuliano.llpinokio@gmail.com>
2020-09-11 12:54:37 -0400

    save_to_csv unnecessary

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,5 +1,5 @@
 from .cacher import Cacher
-from .files import count_flines,save_to_csv
+from .files import count_flines
 from .get_proc_info import get_proc_info,ProcInfo
 from .timer import Timer
 from .progress_meter import ProgressMeter
